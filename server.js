42ad17803aeeee4606c74b4ee0a4297bfc4b1a84
Luis Eduardo <yayohack@hotmail.com>
2015-03-31 15:43:43 -0600

    getRandom function in server.js

--- server.js
+++ server.js
@@ -4,16 +4,31 @@ var express  = require("express"),
     server   = http.createServer(app); 
 
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 app.configure(function () {
+  app.use(allowCrossDomain);
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
 });
 
-app.get('/', function(req, res,next) {
+app.get('/random', function(req, res,next) {
   var rand = Math.random();
-  console.log(rand);
-  res.send("random: " +rand);
+  console.log(rand.toString());
+  res.send(rand.toString());
 });
 
 
